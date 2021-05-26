a16a33372ea877c4b312bb335e42ab4c326a6a85
Hwang Hyejung <jimin_ma@naver.com>
2021-05-26 23:00:47 +0900

    AppBar DropdownButton 제거

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         bottomAppBarColor: Color(0xFF4B30ED),
         textTheme: const TextTheme(
           headline2: TextStyle(
-              color: Colors.black, fontSize: 20, fontWeight: FontWeight.w600),
+              color: Colors.black, fontSize: 19, fontWeight: FontWeight.w600),
         ),
       ),
       home: HomePage(),
@@ -31,41 +31,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String _value = '1';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: DropdownButton<String>(
-          value: _value,
-          icon: const Icon(Icons.keyboard_arrow_down),
-          iconSize: 30,
-          underline: Container(),
-          items: [
-            DropdownMenuItem(
-              child: Text('Delivery to 1600 Amphitheater Way', style: Theme.of(context).textTheme.headline2,),
-              value: '1',
-            ),
-            DropdownMenuItem(
-              child: Text('Delivery to 1800', style: Theme.of(context).textTheme.headline2,),
-              value: '2',
-            ),
-            DropdownMenuItem(
-              child: Text('Delivery to 1800', style: Theme.of(context).textTheme.headline2,),
-              value: '3',
-            ),
-            DropdownMenuItem(
-              child: Text('Delivery to 1800', style: Theme.of(context).textTheme.headline2,),
-              value: '4',
-            ),
-         ],
-          onChanged: (value) {
-           setState(() {
-             _value = value!;
-           });
-          },
-        ),
+        centerTitle: false,
+        elevation: 1.1,
+        title: Text('Delivery to 1600 Amphitheater Way', style: Theme.of(context).textTheme.headline2,),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+            child: Icon(Icons.keyboard_arrow_down, size: 30, color: Color(0xFF4B30ED),),
+          ),
+        ],
       ),
       body: Container(),
     );
